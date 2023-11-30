1cac99c485749843877ff9061371a7e749b0aaea
Mikalai Zoryn <mikalaizoryn@gmail.com>
2023-11-29 20:00:38 -0800

    Initial Commit

---
+++ src/test/java/com/zoryn/java/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoryn.java.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
