ed85ab02557699481ed2158d5ec5ca07563416f5
Mikalai Zoryn <mikalaizoryn@gmail.com>
2023-12-26 16:32:04 -0800

    Added basic flyway integration

---
+++ src/main/resources/db/migration/V1.0.0__Init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ITEM (
+  id            BIGINT NOT NULL PRIMARY KEY,
+  name          VARCHAR(50) NOT NULL,
+  category      VARCHAR(50) NOT NULL,
+  description   VARCHAR(255),
+  rating        DOUBLE PRECISION,
+  price         NUMERIC
+);
