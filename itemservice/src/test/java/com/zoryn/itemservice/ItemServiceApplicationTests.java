436a00c86cd007bcf1998da749db461930a93578
Mikalai Zoryn <mikalaizoryn@gmail.com>
2024-02-16 21:22:33 -0800

    Fixed ItemServiceApplicationTest and change config server url from local to remote

--- itemservice/src/test/java/com/zoryn/itemservice/ItemServiceApplicationTests.java
+++ itemservice/src/test/java/com/zoryn/itemservice/ItemServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.zoryn.itemservice;
 
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ItemServiceApplicationTests {
