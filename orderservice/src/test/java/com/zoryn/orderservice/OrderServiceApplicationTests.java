5a46d616cdca96a49c79dcf6dad6ab18d8979e4f
Mikalai Zoryn <mikalaizoryn@gmail.com>
2024-03-01 00:28:22 -0800

    Fix user service and order service tests

--- orderservice/src/test/java/com/zoryn/orderservice/OrderServiceApplicationTests.java
+++ orderservice/src/test/java/com/zoryn/orderservice/OrderServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.zoryn.itemservice;
+package com.zoryn.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
